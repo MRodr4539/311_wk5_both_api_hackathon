0ab5a57958cc958257e35f3956d3f9162eeacc1f
Jacob <jacobs0408@yahoo.com>
2019-08-07 20:44:10 -0500

    testing

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql')
 const pool = require('../sql/connection')
 const { handleSQLError } = require('../sql/error')
 
@@ -10,24 +11,26 @@ const getEmployees = (req, res) => {
 } 
 
 
-// const getEmployeesById = (req, res) => {
-//     pool.query(`SELECT * FROM employees WHERE ?? = ${req.params.emp_no}`, (err, rows) => {
-//         if (err) return handleSQLError(res, err)
-//         return res.json(rows);
-//     })
-// } 
+const getEmployeesById = (req, res) => {
+    pool.query(`SELECT * FROM employees WHERE emp_no = ${req.params.id}`, (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows);
+    })
+} 
 
-// const getEmployeesByFirstName = () => {
-//     pool.query(`SELECT * FROM employees WHERE ?? = ${req.params.first_name}`, (err, rows) => {
-//         if (err) return handleSQLError(res, err)
-//         return res.json(rows);
-//     })
-// }
-
-
+const getEmployeesByFirstName = (req, res) => {
+    pool.query(`SELECT * FROM employees WHERE first_name = ${req.params.first_name}`, (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows);
+    })
+}
 
 
 
-module.exports = getEmployees;
-    // getEmployeesById, 
-    // getEmployeesByFirstName 
+
+
+module.exports = { 
+    getEmployees,
+    getEmployeesById,
+    getEmployeesByFirstName 
+}
--- routers/employees.js
+++ routers/employees.js
@@ -1,12 +1,12 @@
-
 const express = require('express')
 const employeesController = require('../controllers/employees')
 const router = express.Router()
 
+console.log(employeesController)
 router.get('/', employeesController.getEmployees)
 
-// router.get('/:id', employeeController.getEmployeeById)
+router.get('/:id', employeesController.getEmployeesById)
 
-// router.get('first_name/:first_name')
+router.get('/firstName/:first_name', employeesController.getEmployeesByFirstName)
 
 module.exports = router
